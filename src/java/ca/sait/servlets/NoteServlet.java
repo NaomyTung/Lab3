8f04d8335ebf03b42bb316f5dffdd9947b9f37c5
NaomyTung <106546446+NaomyTung@users.noreply.github.com>
2022-09-27 02:06:16 -0600

    Displays note from the txt file

--- src/java/ca/sait/servlets/NoteServlet.java
+++ src/java/ca/sait/servlets/NoteServlet.java
@@ -4,8 +4,8 @@
  */
 package ca.sait.servlets;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import ca.sait.models.Note;
+import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,7 +33,18 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
         
+        // to read files
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+        
+        String title = br.readLine();
+        String contents = br.readLine();
+
+        Note note = new Note(title, contents);
+        
+        request.setAttribute("note", note);
+        this.getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
     }
 
     /**
@@ -47,6 +58,7 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
         
     }
 
